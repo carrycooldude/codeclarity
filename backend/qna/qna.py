8525cd74d005e186d26701b97afb333e68212de1
Kartikey Rawat <41143496+carrycooldude@users.noreply.github.com>
2025-02-22 17:06:36 +0530

    Delete backend directory

--- backend/qna/qna.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-from raptor_utils import summarize_code
-
-def answer_question(code, question):
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    summary = summarizer(code, max_length=50, min_length=25, do_sample=False)
-    return summary[0]["summary_text"]
